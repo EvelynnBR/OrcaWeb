a7257c726eec0076b1a4f4e3bb9c46d769cdd501
Evelyn <evelynnbr24@gmail.com>
2026-07-08 13:15:54 -0300

    feat: adiciona suporte a className e props no Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,22 @@
-type CardProps = {
+import { twMerge } from "tailwind-merge";
+
+type CardProps = React.ComponentProps<"section"> & {
   children: React.ReactNode;
 };
 
-export default function Card({ children }: CardProps) {
+export default function Card({
+  children,
+  className = "",
+  ...props
+}: CardProps) {
   return (
-    <section className="bg-white border-black shadow-[0_12px_32px_rgba(0,0,0,0.35)] rounded-xl">
+    <section
+      className={twMerge(
+        `bg-white border-black shadow-[0_12px_32px_rgba(0,0,0,0.35)] rounded-xl`,
+        className,
+      )}
+      {...props}
+    >
       {children}
     </section>
   );
