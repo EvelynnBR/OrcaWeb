cc26b77a69e1cc02fb62af15fb9d6ea56024ae75
Evelyn <evelynnbr24@gmail.com>
2026-07-08 17:41:42 -0300

    feat: adiciona rota para a página de orçamento

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import AppLayout from "../components/AppLayout";
 import Home from "../pages/Home";
 import NotFound from "../components/NotFound";
+import Estimate from "../pages/Estimate";
 
 export default function MainRouter() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" index element={<Home />} />
+        <Route path="/orcamento" element={<Estimate />} />
 
         <Route path="*" element={<NotFound />} />
       </Route>
