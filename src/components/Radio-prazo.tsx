2dd4707819b1b50801d2e66de6647b1165c655e8
Evelyn <evelynnbr24@gmail.com>
2026-07-11 00:16:41 -0300

    feat: adicionando componente RadioPrazo com opções de prazo personalizadas

---
+++ src/components/Radio-prazo.tsx
@@ -0,0 +1,79 @@
+import { twMerge } from "tailwind-merge";
+
+import calendar from "../assets/icon/calendar.svg";
+import clock from "../assets/icon/clock.svg";
+import flash from "../assets/icon/flash-default.svg";
+import calendarSelected from "../assets/icon/calendar-selected.svg";
+import clockSelected from "../assets/icon/clock-selected.svg";
+import flashSelected from "../assets/icon/flash.svg";
+import { useState } from "react";
+
+type Prazo = {
+  icon: string;
+  iconSelected: string;
+  value: string;
+  label: string;
+};
+
+type PrazoProps = {
+  onChange: (prazo: Prazo) => void;
+};
+
+const prazo: Prazo[] = [
+  {
+    icon: calendar,
+    iconSelected: calendarSelected,
+    value: "flexivel",
+    label: "Flexível",
+  },
+  {
+    icon: clock,
+    iconSelected: clockSelected,
+    value: "normal",
+    label: "Normal",
+  },
+  {
+    icon: flash,
+    iconSelected: flashSelected,
+    value: "urgente",
+    label: "Urgente",
+  },
+];
+
+export default function RadioPrazo({ onChange }: PrazoProps) {
+  const [selectedPrazo, setSelectedPrazo] = useState<string | null>(null);
+  return (
+    <section className="grid md:flex gap-4 md:justify-between">
+      {prazo.map((pra) => {
+        const isSelected = selectedPrazo === pra.value;
+        return (
+          <label
+            key={pra.value}
+            className={twMerge(
+              `flex relative gap-2 p-2 group lg:hover:border-purple-700 lg:hover:text-purple-700 justify-center items-center bg-purple border rounded-md border-gray-500 md:min-w-50 lg:w-60 ${isSelected && "border-purple-700 text-purple-700"}`,
+            )}
+          >
+            <img
+              src={isSelected ? pra.iconSelected : pra.icon}
+              alt=""
+              aria-hidden="true"
+              className={`grid`}
+            />
+            <input
+              type="radio"
+              name="prazo"
+              value={pra.value}
+              checked={isSelected}
+              onChange={() => {
+                setSelectedPrazo(pra.value);
+                onChange(pra);
+              }}
+              className="absolute inset-0 opacity-0 lg:cursor-pointer"
+            />
+            <span>{pra.label}</span>
+          </label>
+        );
+      })}
+    </section>
+  );
+}
