31104c036e986b44c9bf6fb7af3aeaf114a975aa
Evelyn <evelynnbr24@gmail.com>
2026-07-13 05:18:18 -0300

    feat: refatora o componente Checked para melhorar o gerenciamento de estado e o tratamento das props

--- src/components/Checked.tsx
+++ src/components/Checked.tsx
@@ -2,62 +2,50 @@ import { useState } from "react";
 import checkedbox from "../assets/icon/checkedbox.svg";
 import { twMerge } from "tailwind-merge";
 
-type Checked = {
+type CheckedItem = {
   value: string;
   label: string;
 };
-type OnChecked = {
-  onChange: (check: Checked) => void;
+
+type CheckedProps = CheckedItem & {
+  onChange: (item: CheckedItem, checked: boolean) => void;
 };
 
-const check = [
-  {
-    value: "formContact",
-    label: "Formulário de Contato",
-  },
-  {
-    value: "blog",
-    label: "Blog",
-  },
-  {
-    value: "seo",
-    label: "SEO básico",
-  },
-];
+export default function Checked({ value, label, onChange }: CheckedProps) {
+  const [isChecked, setIsChecked] = useState(false);
 
-export default function Checked({ onChange }: OnChecked) {
-  const [isChecked, setIsChecked] = useState<string | null>(null);
-  const [onValues, setOnValues] = useState<string[]>([]);
+  function handleChange() {
+    const newCheckedState = !isChecked;
+
+    setIsChecked(newCheckedState);
+
+    onChange(
+      {
+        value,
+        label,
+      },
+      newCheckedState,
+    );
+  }
+
   return (
     <section className="grid gap-2">
-      {check.map((c) => {
-        const isSelected = isChecked === c.value;
-        const isValues = onValues.includes(c.value);
-        return (
-          <label key={c.value} className="relative flex gap-3 items-center">
-            <div className="border border-gray-400 w-5 h-5 bg-purple-600 rounded-md hover:border-purple-600 hover:cursor-pointer">
-              <img
-                src={isSelected ? checkedbox : ""}
-                alt=""
-                className={twMerge(`w-full `)}
-              />
-            </div>
-            <input
-              className="absolute inset-0 opacity-0"
-              type="checkbox"
-              name="functionality"
-              value={c.value}
-              checked={isValues}
-              onChange={() => {
-                setIsChecked(c.value);
-                setOnValues;
-                onChange(c);
-              }}
-            />
-            <span>{c.label}</span>
-          </label>
-        );
-      })}
+      <label className="relative flex gap-3 items-center">
+        <div className="border border-gray-400 w-5 h-5 bg-purple-600 rounded-md hover:border-purple-600 hover:cursor-pointer">
+          {isChecked && (
+            <img src={checkedbox} alt="" className={twMerge(`w-full`)} />
+          )}
+        </div>
+        <input
+          className="absolute inset-0 opacity-0"
+          type="checkbox"
+          name="functionality"
+          value={value}
+          checked={isChecked}
+          onChange={handleChange}
+        />
+        <span>{label}</span>
+      </label>
     </section>
   );
 }
